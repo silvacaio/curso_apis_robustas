dd4b6404bc446ad2d0d53ad1c09671e3fd7d4312
Caio Silva <silva.caiosfc@hotmail.com>
2018-01-16 13:11:17 -0200

    Add project files.

---
+++ XGame.Domain/Enums/EnumStatusJogador.cs
@@ -0,0 +1,9 @@
+﻿namespace XGame.Domain.Enums
+{
+    public enum EnumStatusJogador
+    {
+        Ativo = 1,
+        Bloqueado = 2,
+        EmAndamento = 0
+    }
+}
---
+++ XGame.Domain/ValueObjects/Nome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XGame.Domain.ValueObjects
+{
+    public class Nome
+    {
+        public string PrimeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+    }
+}
